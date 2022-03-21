9f9d90cc3e3954984dd93887f4453042cba6e399
iXmalMonk <ixmal999@gmail.com>
2022-03-21 15:40:57 +0300

    drop

--- SDL2_practice_02_Manastyrskij/SDL2_practice_02_Manastyrskij.cpp
+++ SDL2_practice_02_Manastyrskij/SDL2_practice_02_Manastyrskij.cpp
@@ -9,14 +9,20 @@ int w = 900, h = 900;
 
 float radian = M_PI / 180.0;
 
-struct Drop {
-	float x1;
-	float y1;
-	float x2;
-	float y2;
+struct Color {
+	int red;
+	int green;
+	int blue;
 };
 
-enum DirectionSnake { up, down, left, right};
+struct Drop {
+	float x;
+	float y;
+	float speed;
+	Color color;
+};
+
+enum DirectionSnake { up, down, left, right };
 
 void DeInit(int error)
 {
@@ -52,53 +58,55 @@ void Init()
 	}
 }
 
-void GenerateDrop(Drop drop[], int i, float drop_angle, float drop_length)
+void CreateDrop(const int drop_count, Drop drop[])
 {
-	if (rand() % 2 == 0)
-	{
-		drop[i].x1 = rand() % (w+1);
-		drop[i].y1 = 0.0;
-	}
-	else
-	{
-		drop[i].x1 = 0.0;
-		drop[i].y1 = rand() % (h+1);
-	}
-
-	drop[i].x2 = drop[i].x1 - cos(drop_angle * radian) * drop_length;
-	drop[i].y2 = drop[i].y1 - sin(drop_angle * radian) * drop_length;
-}
-
-void Drops(const int drop_count, Drop* drop, float drop_speed, float drop_angle, float drop_length)
-{
-	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-
 	for (int i = 0; i < drop_count; i++)
 	{
-		SDL_RenderDrawLine(renderer, drop[i].x1, drop[i].y1, drop[i].x2, drop[i].y2);
+		drop[i].x = rand() % w;
+		drop[i].y = rand() % h;
+		drop[i].speed = rand() % 26 + 25; // 25 or 50;
+		drop[i].color.red = rand() % 76; // 0 or 75
+		drop[i].color.green = rand() % 76; // 0 or 75
+		drop[i].color.blue = rand() % 56 + 200; // 200 or 255;
+	}
+}
 
-		drop[i].x1 += drop_speed;
-		drop[i].x2 += drop_speed;
-		drop[i].y1 += drop_speed;
-		drop[i].y2 += drop_speed;
+void DrawDrop(const int drop_count, Drop drop[], float drop_angle, float drop_length, float drop_speed_scale)
+{
+	for (int i = 0; i < drop_count; i++)
+	{
+		SDL_SetRenderDrawColor(renderer, drop[i].color.red, drop[i].color.green, drop[i].color.blue, 255);
+		SDL_RenderDrawLine(renderer, drop[i].x, drop[i].y, drop[i].x - cos(drop_angle * radian) * drop_length, drop[i].y - sin(drop_angle * radian) * drop_length);
 
-		if (drop[i].x1 >= w or drop[i].y1 >= h) GenerateDrop(drop, i, drop_angle, drop_length);
+		drop[i].x += cos(drop_angle * radian) * drop[i].speed * drop_speed_scale;
+		drop[i].y += sin(drop_angle * radian) * drop[i].speed * drop_speed_scale;
+
+		if (drop[i].x - cos(drop_angle * radian) * drop_length >= w)
+			drop[i].x = 0;
+		else if (drop[i].x - cos(drop_angle * radian) * drop_length <= -w)
+			drop[i].x = w;
+		else if (drop[i].y - sin(drop_angle * radian) * drop_length >= h)
+			drop[i].y = 0;
+		else if (drop[i].y - sin(drop_angle * radian) * drop_length <= -h)
+			drop[i].y = h;
 	}
 }
 
 void GenerateStartPositionForSnakeOnCenter(SDL_Rect snake[], int snake_size, int snake_scale)
 {
-	for (int i = 0, j = 0; i < snake_size; i++, j+= snake_scale)
+	for (int i = 0, j = 0; i < snake_size; i++, j += snake_scale)
 	{
-		snake[i].x = w/2 - snake_scale / 2;
-		snake[i].y = h/2 - snake_scale / 2 + j;
+		snake[i].x = w / 2 - snake_scale / 2;
+		snake[i].y = h / 2 - snake_scale / 2 + j;
 		snake[i].w = snake_scale;
 		snake[i].h = snake_scale;
 	}
 }
 
-void Snake(const Uint8* keyboard, const int snake_size, SDL_Rect* snake, SDL_Rect* snake_temp, DirectionSnake& snake_direction, int snake_scale, const int snake_delay, int& snake_delay_counter)
+void Snake(const Uint8* keyboard, const int snake_delay, const int snake_size, SDL_Rect* snake, SDL_Rect* snake_temp, DirectionSnake& snake_direction, int snake_scale)
 {
+	static int snake_delay_counter = 0;
+
 	if (keyboard[SDL_SCANCODE_UP] and snake_direction != down) snake_direction = up;
 	if (keyboard[SDL_SCANCODE_DOWN] and snake_direction != up) snake_direction = down;
 	if (keyboard[SDL_SCANCODE_LEFT] and snake_direction != right) snake_direction = left;
@@ -141,7 +149,7 @@ void Snake(const Uint8* keyboard, const int snake_size, SDL_Rect* snake, SDL_Rec
 	}
 	snake_delay_counter++;
 
-	
+
 	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
 	for (int i = 0; i < snake_size; i++)
 		SDL_RenderFillRect(renderer, &snake[i]);
@@ -170,11 +178,11 @@ int main(int argc, char* argv[])
 {
 	Init();
 
-	#pragma region VAR
+#pragma region VAR
 	bool launched = true;
 	const int fps = 30;
-	SDL_Event event;
 	const Uint8* keyboard = SDL_GetKeyboardState(NULL);
+	SDL_Event event;
 
 	bool HideConsole = true;
 	if (HideConsole)
@@ -184,45 +192,40 @@ int main(int argc, char* argv[])
 		HC = FindWindowA("ConsoleWindowClass", NULL);
 		ShowWindow(HC, 0);
 	}
-	#pragma endregion
+#pragma endregion
 
-	#pragma region VAR DROPS
-	const int drop_count = 50;
+#pragma region VAR DROPS
+	const int drop_count = 100;
 	Drop drop[drop_count];
-	float drop_speed = 25.0;
-	float drop_angle = 45.0;
+	float drop_angle = 60.0;
 	float drop_length = 100.0;
-	
-	for (int i = 0; i < drop_count; i++)
-		GenerateDrop(drop, i, drop_angle, drop_length);
-	#pragma endregion
+	float drop_speed_scale = 1.0;
+	CreateDrop(drop_count, drop);
+#pragma endregion
 
-	#pragma region VAR SNAKE
+#pragma region VAR SNAKE
+	const int snake_delay = 5;
 	const int snake_size = 3;
 	SDL_Rect snake[snake_size];
 	SDL_Rect snake_temp[snake_size - 1];
 	DirectionSnake snake_direction = up;
 	int snake_scale = 20;
-
-	const int snake_delay = 5;
-	int snake_delay_counter = 0;
-
 	GenerateStartPositionForSnakeOnCenter(snake, snake_size, snake_scale);
-	#pragma endregion
+#pragma endregion
 
 	while (launched)
 	{
-	Event(event, launched);
+		Event(event, launched);
 
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-	SDL_RenderClear(renderer);
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
 
-	Drops(drop_count, drop, drop_speed, drop_angle, drop_length);
+		DrawDrop(drop_count, drop, drop_angle, drop_length, drop_speed_scale);
 
-	Snake(keyboard, snake_size, snake, snake_temp, snake_direction, snake_scale, snake_delay, snake_delay_counter);
+		Snake(keyboard, snake_delay, snake_size, snake, snake_temp, snake_direction, snake_scale);
 
-	SDL_RenderPresent(renderer);
-	SDL_Delay(1000 / fps);
+		SDL_RenderPresent(renderer);
+		SDL_Delay(1000 / fps);
 	}
 
 	DeInit(0);
